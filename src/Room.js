b52ea8596961dc01d00c5985a1df91faf8e0b5d0
wassay76 <ab.wassey.76@gmail.com>
2020-11-28 19:00:22 +0500

    react update

--- src/Room.js
+++ src/Room.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Room() {
+    //const state = useState(true);
+   // console.log("state = ",state)
+   let [islit , setlit] = useState(true);
+   let [age, setage]= useState(24);
+   function updatelit(){
+    console.log("button clicked")
+    setlit(!islit); 
+  
+  }
+   const increament_age = ()=>{
+    console.log("button clicked")
+    setage(++age); 
+  
+  }  
+
+
   return (
     <div>
-      this message is from room 
+      the room is {islit? "lit": "dark"}
+    <br/>
+    <button onClick={updatelit}>toogle light</button>
+    <br/>
+    AGE = {age}
+    <br/>
+    <button onClick={increament_age}>increase age </button>
+
+    
     </div>
+
   );
 }
 
